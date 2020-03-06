0c37163d451909997f4f6be8c46ac23b1ea6c83e
Ibrahim <ibrahim.tigrek@gmail.com>
2020-03-06 20:06:45 +0300

    updated multiple_user_tweets.py

--- multiple_users_tweets.py
+++ multiple_users_tweets.py
@@ -11,11 +11,11 @@ ACCESS_TOKEN = AuthInfo.ACCESS_TOKEN
 ACCESS_TOKEN_SECRET = AuthInfo.ACCESS_TOKEN_SECRET
 
 # JSON
-INPUT_FILENAME = "out/eng_usernames.txt"
-# OUTPUT_FILENAME = "out/multiple_user_tweets_test.json"
-
+INPUT_FILENAME = "eng_usernames.txt"
+OUTPUT_FILENAME = "tweets.json"
 
 def get_timeline_tweets(api, username):
+    ''' retrieves ~3k tweets from specified user '''
     user_tweets = tweepy.Cursor(api.user_timeline, id=username, tweet_mode='extended')
 
     print(f"getting tweets for {username}")
@@ -27,9 +27,7 @@ def get_timeline_tweets(api, username):
         for tweet in user_tweets.items():
             tweet_id = tweet.id_str
             full_text = tweet.full_text
-
             tweets[tweet_id] = full_text
-
             counter += 1
             if counter % 100 == 0:
                 print(".", end="")
@@ -42,48 +40,36 @@ def get_timeline_tweets(api, username):
     print(f"\nCollected {counter} tweets [{time.time() - start_time:.2f} s]")
     return tweets
 
-if __name__ == "__main__":
+def get_api():
+    ''' setup tweepy auth '''
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    return tweepy.API(auth)
 
-    # final_user = None
-    left_off_line = 114 # 65
-    file_counter = 17
+if __name__ == "__main__":  # main function
+    sleep_duration = 10 # sec
 
-    while True:
-        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-        api = tweepy.API(auth)
-        with open(INPUT_FILENAME) as in_f:
-
-            if left_off_line:
-                for _ in range(left_off_line - 1):
-                    in_f.readline()
-                current_line = left_off_line - 1
-            else:
-                current_line = -1
-
-            all_tweets = {}
-
-            for line in in_f:
-                current_line += 1
-                username = line.strip("\n")
+    api = get_api()
+    all_tweets = {}
+    with open(INPUT_FILENAME) as file:
+        for line in file:
+            username = line.strip("\n")
+            while True:
+            # this loop allows the stream to reset after blocked by twitter API
                 tweets = get_timeline_tweets(api, username)
-                if tweets is False:
-                    left_off_line = current_line
-                    print(f"stopped at {username}, {left_off_line}")
-                    break
-                elif len(tweets) > 0:
+                if tweets and len(tweets) > 0:
                     all_tweets[username] = tweets
+                    break
 
-        if len(all_tweets) > 0:
-            print(f"\nsaving users' tweets in tweets{file_counter}.json\n")
-            with open(f"out/tweets/tweets{file_counter}.json", "w") as out_f:
-                out_f.write(json.dumps(all_tweets))
-            file_counter += 1
-        else:
-            break
+                print(f"sleeping for {sleep_duration} seconds")
+                time.sleep(sleep_duration)
+                api = get_api() # reset api
 
-        sleep_duration = 240 # sec
-        print(f"sleeping for {sleep_duration} seconds")
-        time.sleep(sleep_duration) # maximum 240 seconds
+    print(f"\nsaving users' tweets in {OUTPUT_FILENAME}\n")
+    with open(OUTPUT_FILENAME, "w") as file:
+        file.write(json.dumps(all_tweets))
+
+
+        
     
     
